3527d971f8a8610cb436c7238722f54e0b823735
enhouse <elizabeth.house88@gmail.com>
2023-09-10 17:30:53 -0400

    calculates

--- project_miles_to_km.py
+++ project_miles_to_km.py
@@ -1,5 +1,19 @@
 from tkinter import *
 
+
+# conversion function
+def start_calculation():
+    # obtain user input validates this is a number
+    try:
+        miles = float(miles_input.get())
+        result = 1.609*miles
+        calculation.config(text=result)
+    except(ValueError):
+        # clears entry and replaces with prompt enter a number
+        miles_input.delete(0,END)
+        miles_input.insert(END, "Enter A Number")
+
+
 # create layout
 window = Tk()
 window.title("Miles to Km Converter")
@@ -8,6 +22,7 @@ window.minsize(width=200, height=100)
 
 miles_input = Entry()
 miles_input.grid(column=1, row=0)
+miles_input.focus()
 
 miles_label = Label(text="Miles")
 miles_label.grid(column=2, row=0)
@@ -22,10 +37,8 @@ kms_label = Label(text="Km")
 kms_label.grid(column=2, row=1)
 kms_label.config(pady=10)
 
-
 calc_button = Button(text="Calculate")
 calc_button.grid(column=1, row=2)
-# calc_button.config(padx=3)
-
+calc_button.config(command=start_calculation)
 
 window.mainloop()
